97642ddcd885006b129d1625a9f67d8f7b60300d
Tkkg1994 <luca.grifo@outlook.com>
2016-05-30 03:44:50 +0200

    adding many new cpufreqs: hyper, alucard, barry_allen, bioshock, blu_active, conservative_x, darkness, despair, electrodemand, impulse, intellicative, intellidemand, intellimm, interactive_pro, interactive_x, ironactive, lionheart, nightmare, ondemand_x, ondemandplus, pegasusq, smartass2, smartmax, smartmax_eps, yankactive and zzmoove
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>
    Signed-off-by: theophile <chmeredith@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	drivers/cpufreq/cpufreq.c
    	drivers/cpufreq/exynos-mp-cpufreq.c
    	include/linux/cpufreq.h

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1383,6 +1383,36 @@ const char *cpufreq_get_current_driver(void)
 }
 EXPORT_SYMBOL_GPL(cpufreq_get_current_driver);
 
+/**
+ *	cpufreq_get_driver_data - return current driver data
+ *
+ *	Return the private data of the currently loaded cpufreq
+ *	driver, or NULL if no cpufreq driver is loaded.
+ */
+void *cpufreq_get_driver_data(void)
+{
+	if (cpufreq_driver)
+		return cpufreq_driver->driver_data;
+
+	return NULL;
+}
+EXPORT_SYMBOL_GPL(cpufreq_get_driver_data);
+
+/**
+ * cpufreq_notify_utilization - notify CPU userspace about CPU utilization
+ * change
+ *
+ * This function is called everytime the CPU load is evaluated by the
+ * ondemand governor. It notifies userspace of cpu load changes via sysfs.
+ */
+void cpufreq_notify_utilization(struct cpufreq_policy *policy,
+		unsigned int util)
+{
+	if (policy) {
+		policy->util = util;
+	}
+}
+
 /*********************************************************************
  *                     NOTIFIER LISTS INTERFACE                      *
  *********************************************************************/
